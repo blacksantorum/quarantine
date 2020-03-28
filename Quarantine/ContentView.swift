792bdc534d19481cbd292f505d919b9d1492ede1
Chris Tibbs <eggyeggbean@google.com>
2020-03-28 11:24:21 -0400

    Initial Commit

---
+++ Quarantine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quarantine
+//
+//  Created by Chris Tibbs on 3/28/20.
+//  Copyright © 2020 Chris Tibbs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
